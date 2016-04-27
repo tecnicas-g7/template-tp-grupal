10f525f156fbc2d6c083f6ce5007ab370bff2514
HikaruKirishima <abril14>
2016-04-27 00:27:39 -0300

    Drop Action + Test with condition 2 items max and two rules to win

--- src/test/java/ar/fiuba/tdd/tp/MainTests.java
+++ src/test/java/ar/fiuba/tdd/tp/MainTests.java
@@ -1,18 +1,56 @@
 package ar.fiuba.tdd.tp;
 
 import ar.fiuba.tdd.tp.game.*;
-
+import ar.fiuba.tdd.tp.game.actions.DropAction;
 import ar.fiuba.tdd.tp.game.actions.PickAction;
+import ar.fiuba.tdd.tp.game.conditions.InventoryCondition;
+import ar.fiuba.tdd.tp.game.conditions.RoomCondition;
 import ar.fiuba.tdd.tp.game.items.Item;
 import ar.fiuba.tdd.tp.game.types.EnterRoom;
 import ar.fiuba.tdd.tp.game.types.StickGame;
+
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class MainTests {
+
+    private Item createItemsWithCondition(String nameItem) {
+        Item itemWithActions = new Item(nameItem);
+        itemWithActions.addAction(new PickAction());
+        itemWithActions.addAction(new DropAction());
+        return itemWithActions;
+    }
+
+    private Game getGameCondition() {
+        Item key = createItemsWithCondition("key");
+        Item mouse = createItemsWithCondition("mouse");
+        Item stick = createItemsWithCondition("stick");
+        Room room1 = new Room("Room1");
+        room1.addItem(key);
+        room1.addItem(mouse);
+        room1.addItem(stick);
+        Room room2 = new Room("Room2");
+        Player player = new Player(room1);
+        player.setMaxInventory(2);
+        room1.addDoor(room2,key);
+        room2.addDoor(room1,key);
+        Game game = new Game(player);
+        game.addRoom(room1);
+        game.addRoom(room2);
+        List<Item> items = new ArrayList<>(Arrays.asList(stick,key));
+        // items.add(stick);
+        //  items.add(key);
+        game.addCondition(new InventoryCondition(items));
+        game.addCondition(new RoomCondition(room2,true));
+        return game;
+    }
 
     @Test
     public void dummy_test() {
@@ -100,5 +138,30 @@ public class MainTests {
         player.enter(door);
 
         assertFalse(player.checkVictory(player2));
+    }
+
+    @Test
+    public void cantTakeMoreItems() {
+        Game dropGame = getGameCondition();
+        Controller controller = new Controller(dropGame);
+        try {
+            String command = "pick key";
+            controller.interptetCommand(command);
+            command = "pick mouse";
+            controller.interptetCommand(command);
+            command = "pick stick";
+            controller.interptetCommand(command);
+            command = "drop mouse";
+            controller.interptetCommand(command);
+            command = "pick stick";
+            controller.interptetCommand(command);
+            command = "enter door1";
+            controller.interptetCommand(command);
+
+        } catch (Exception e) {
+            //Do nothing
+        }
+
+        assertTrue(dropGame.verifyVictory());
     }
 }
