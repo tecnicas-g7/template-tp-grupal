df549a01edf97994d5f936e01203320836c266d7
leandro <ltessore@github.com>
2016-04-28 01:49:58 -0300

    Acomodo como los demas juegos

--- src/main/java/ar/fiuba/tdd/tp/game/types/HanoiTower.java
+++ src/main/java/ar/fiuba/tdd/tp/game/types/HanoiTower.java
@@ -2,22 +2,19 @@ package ar.fiuba.tdd.tp.game.types;
 
 import ar.fiuba.tdd.tp.exceptions.MaxInventoryException;
 import ar.fiuba.tdd.tp.game.*;
-import ar.fiuba.tdd.tp.game.conditions.Condition;
+
 import ar.fiuba.tdd.tp.game.items.Item;
 
 import java.util.Iterator;
-import java.util.List;
+
 
 /**
  * Created by ltessore on 27/04/16.
  */
 public class HanoiTower {
-    private Room room1;
-    private Player player;
-    private List<Condition> restrictionList;
 
-    public HanoiTower() {
-        this.room1 = new Room("R1");
+    public static Game getGame() {
+        Room room1 = new Room("R1");
 
         Item pieza1 = new Item("1");
         Item pieza2 = new Item("2");
@@ -36,25 +33,22 @@ public class HanoiTower {
         room1.addContainerComponent(stack2);
         room1.addContainerComponent(stack3);
 
-        this.player = new Player(room1);
+        Player player = new Player(room1);
+        player.setMaxInventory(1);
+        Game game = new Game(player);
 
-        this.restrictionList = addRestrictions();
+        return game;
+
 
     }
 
-    private List<Condition> addRestrictions() {
-        List<Condition> conditions = null;
-        //TODO Agregar restricciones de movimiento
-        return conditions;
-    }
-
-    private void move(ContainerComponent stackFrom, ContainerComponent stackAfter) throws MaxInventoryException {
+    private void move(Player player, ContainerComponent stackFrom, ContainerComponent stackAfter) throws MaxInventoryException {
         ContainerComponent item = stackAfter.getLast();
         //Player toma pieza
-        this.player.addItem(item);
+        player.addItem(item);
 
         //Mover
-        if (isMovementValid(stackAfter)) {
+        if (isMovementValid(player, stackAfter)) {
             System.out.print(" MOVIMIENTO VALIDO \n");
             stackFrom.removeComponent(item);
             stackAfter.addComponent(item);
@@ -62,13 +56,13 @@ public class HanoiTower {
             System.out.print(" MOVIMIENTO INVALIDO \n");
         }
         //Player deja pieza
-        this.player.removeItem(item.getName());
+        player.removeItem(item.getName());
 
     }
 
-    private Boolean isMovementValid(ContainerComponent stackAfter) {
+    private Boolean isMovementValid(Player player, ContainerComponent stackAfter) {
         // chequea condiciones de movimiento de la pieza
-        Iterator<ContainerComponent> it = this.player.getInventoryIterator();
+        Iterator<ContainerComponent> it = player.getInventoryIterator();
         //chequea si el ultimo del stackAfter es mas grande del que tiene el player
         if (Integer.parseInt(stackAfter.getLast().getName()) > Integer.parseInt(it.next().getName())) {
             return true;
@@ -78,23 +72,22 @@ public class HanoiTower {
 
 
 
-    public void play() throws MaxInventoryException{
+    public void play(Player player) throws MaxInventoryException{
 
-        move(room1.getItem("stack1"), room1.getItem("stack2"));
+        move(player,player.getRoom().getItem("stack1"), player.getRoom().getItem("stack2"));
 
-        move(room1.getItem("stack1"), room1.getItem("stack3"));
+        move(player,player.getRoom().getItem("stack1"), player.getRoom().getItem("stack3"));
 
-        move(room1.getItem("stack2"), room1.getItem("stack3"));
+        move(player,player.getRoom().getItem("stack2"),player.getRoom().getItem("stack3"));
 
-        move(room1.getItem("stack1"), room1.getItem("stack2"));
+        move(player,player.getRoom().getItem("stack1"), player.getRoom().getItem("stack2"));
 
-        move(room1.getItem("stack3"), room1.getItem("stack1"));
+        move(player,player.getRoom().getItem("stack3"),player.getRoom().getItem("stack1"));
 
-        move(room1.getItem("stack3"), room1.getItem("stack2"));
+        move(player,player.getRoom().getItem("stack3"),player.getRoom().getItem("stack2"));
 
-        move(room1.getItem("stack1"), room1.getItem("stack2"));
-
-
+        move(player,player.getRoom().getItem("stack1"),player.getRoom().getItem("stack2"));
+        
         System.out.print("FIN");
     }
 
