0115df672e93b17f8fb749f00bb5693d6bbc2e17
NicoGallegos <nicolas_gallegos@hotmail.com>
2016-05-19 00:51:54 -0300

    Agrego clases faltantes de merge

--- src/main/java/ar/fiuba/tdd/tp/driver/GameDriver.java
+++ src/main/java/ar/fiuba/tdd/tp/driver/GameDriver.java
@@ -1,7 +1,8 @@
 package ar.fiuba.tdd.tp.driver;
 
 public interface GameDriver {
-    void initGame(String jarPath);
 
+    void initGame(String jarPath);
     String sendCommand(String cmd);
+
 }
---
+++ src/main/java/ar/fiuba/tdd/tp/server/BuilderLoader.java
@@ -0,0 +1,66 @@
+package ar.fiuba.tdd.tp.server;
+
+
+import model.GameBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+public class BuilderLoader {
+
+    private static String escape(String name) {
+        if (name.contains("/")) {
+            name = name.replaceAll("/", ".");
+        }
+        if (name.contains("\\")) {
+            name = name.replaceAll("\\\\", ".");
+        }
+        return name;
+    }
+
+    private static String processEntry(JarEntry entry) {
+        String name = entry.getName();
+        return escape(name.substring(0,name.lastIndexOf(".class")));
+    }
+
+    private static List<String> scanJar(File file) throws IOException, IllegalArgumentException {
+        if (file == null || !file.exists() || !file.getName().endsWith(".jar")) {
+            throw new IllegalArgumentException("file");
+        }
+
+        List<String> foundClasses = new ArrayList<>();
+        try (JarFile jarFile = new JarFile(file)) {
+            jarFile.stream()
+                    .filter(e -> e.getName().endsWith(".class"))
+                    .forEach(e -> foundClasses.add(processEntry(e)));
+        }
+        return foundClasses;
+    }
+
+    public static GameBuilder load(String filePath)
+            throws ClassNotFoundException, IOException,
+            IllegalAccessException, InstantiationException {
+        File file = new File(filePath);
+        URL[] urls = { new URL("jar:file:" + filePath + "!/") };
+        ClassLoader loader = URLClassLoader.newInstance(urls);
+        for (String classFile : scanJar(file)) {
+            Class<?> foundClass;
+            if (loader == null) {
+                foundClass = Class.forName(classFile);
+            } else {
+                foundClass = Class.forName(classFile, true, loader);
+            }
+
+            if (GameBuilder.class.isAssignableFrom(foundClass) && !foundClass.equals(GameBuilder.class)) {
+                return (GameBuilder)foundClass.newInstance();
+            }
+        }
+        return null;
+    }
+}
