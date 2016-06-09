76b16a1d6d335dd616f6e320e145601e24648509
javier <javschulz@gmail.com>
2016-06-09 19:09:34 -0300

    fix

--- core/src/main/java/game/conditions/isPlayerCondition.java
+++ core/src/main/java/game/conditions/isPlayerCondition.java
@@ -1,7 +1,20 @@
 package game.conditions;
 
+import game.Player;
+
 /**
- * Created by javier on 6/9/16.
+ Created by javier on 6/9/16.
  */
-public class isPlayerCondition {
+public class isPlayerCondition implements Condition {
+
+    Player player;
+
+    public isPlayerCondition(Player player) {
+        this.player = player;
+    }
+
+    @Override
+    public boolean isValid(Player player) {
+        return player.equals(this.player);
+    }
 }
